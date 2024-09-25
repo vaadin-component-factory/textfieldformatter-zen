75f1955848060a8a82dfe13c7517adb2d7308c87
Drew Harvey <drew@vaadin.com>
2024-09-25 15:17:31 -0700

    allowing toggle back and forth in replacing mask test view

--- src/test/java/org/vaadin/formatter/ZenCSBFReplacingMaskUI.java
+++ src/test/java/org/vaadin/formatter/ZenCSBFReplacingMaskUI.java
@@ -4,7 +4,6 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.data.value.ValueChangeMode;
 import org.vaadin.textfieldformatter.AbstractTest;
 import org.vaadin.textfieldformatter.UITestConfiguration;
 
@@ -16,30 +15,28 @@ public class ZenCSBFReplacingMaskUI extends AbstractTest {
             false);
     private ZenCustomStringBlockFormatter current;
     private TextField textField;
+    private boolean isFirstFormat = true;
 
     @Override
     public Component getTestComponent(UITestConfiguration configuration) {
         VerticalLayout layout = new VerticalLayout();
         textField = new TextField();
-        textField.setValueChangeMode(ValueChangeMode.EAGER);
         current = new ZenCustomStringBlockFormatter(formatterA);
         current.extend(textField);
 
-        Button toggleButton = new Button("SWITCH", event -> {
-            replaceFormatter(textField, formatterB);
-        });
+        Button toggleButton = new Button("SWITCH", event -> replaceFormatter());
         toggleButton.setId("switch");
-        toggleButton.setDisableOnClick(true);
 
         layout.add(textField, toggleButton);
         return layout;
     }
 
-    private void replaceFormatter(TextField textField, ZenCustomStringBlockFormatter.Options options) {
+    private void replaceFormatter() {
+        isFirstFormat = !isFirstFormat;
         if (current != null) {
             current.remove();
         }
-        current = new ZenCustomStringBlockFormatter(options);
+        current = new ZenCustomStringBlockFormatter(isFirstFormat ? formatterA : formatterB);
         current.extend(textField);
     }
 }
