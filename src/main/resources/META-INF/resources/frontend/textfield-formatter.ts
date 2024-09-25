3cdecede13ba69968a39e82dea6e8b7abc09cfab
Drew Harvey <drew@vaadin.com>
2024-09-25 15:08:16 -0700

    Updating overarching flow in frontend logic for vaadin text field/area (see below)
    Formatting always happens immediately:
    - formatting now always happens on input
    - the normal input event handler no longer notifies vaadin component of changes
    
    When server receives changes is based on the provided ValueChangeMode:
    - added dedicated listener that notifies vaadin component of changes (this listener's event is based on what the server passes down, which is based on the ValueChangeMode of the component)
    
    Listeners get cleaned up:
    - listeners are automatically removed in the disconnectCallback method
    
    An input event is fired when cleave config is updated:
    - a manual input event is fired whenever the updateConf (happens from the server side) is called
    - this fixes two things: prefixes are now displayed immediately and changing the config while a component already has a value will immediately update the value
    - possible issue here is that it will trigger a value change event on the server
    
    Added more logging:
    - added log prefix for easier filtering in Chrome dev tools console
    - added more logs for debugging during dev

--- src/main/resources/META-INF/resources/frontend/textfield-formatter.ts
+++ src/main/resources/META-INF/resources/frontend/textfield-formatter.ts
@@ -31,6 +31,8 @@ const stripDelimiters = (
 class TextfieldFormatter extends HTMLElement {
   static get is() { return 'textfield-formatter'; }
 
+  private logPrefix: string = 'CleaveZen: ';
+
   private configuration: any;
   private valueChangeEvent: string = 'input';
   private cursorTracker?: CursorTrackerDestructor;
@@ -38,44 +40,77 @@ class TextfieldFormatter extends HTMLElement {
   private formatType: 'creditCard' | 'general' | 'numeral' | 'date'  | 'time' = 'creditCard';
 
   connectedCallback() {
+    console.log(this.logPrefix + "connectedCallback");
+    this.findInputElement();
+    this.addListeners();
+  }
+
+  findInputElement() {
     this.el = this.parentElement!.shadowRoot!.querySelector('input'); // retrocompatibility purposes
-    if(!this.el) {
+    if (!this.el) {
       this.el = this.parentElement!.querySelector('input');
     }
-    if (this.el) {
-      this.el!.addEventListener('input', this.creditTypeChanged());
-      if (['VAADIN-TEXT-FIELD', 'VAADIN-TEXT-AREA'].includes(this.parentElement!.tagName.toUpperCase())) {
-        this.el!.addEventListener(this.valueChangeEvent, this.inputChanged());
-      } else {
-        this.el!.addEventListener('change', this.inputValueChanged());
-      }
+  }
+
+  addListeners() {
+    console.log(this.logPrefix + "Adding change listeners");
+
+    if (!this.el) {
+      console.error(this.logPrefix + "Cannot add listeners because this.el is null");
+      return;
+    }
+
+    this.el!.addEventListener('input', this.creditTypeChanged);
+    if (['VAADIN-TEXT-FIELD', 'VAADIN-TEXT-AREA'].includes(this.parentElement!.tagName.toUpperCase())) {
+      this.el!.addEventListener('input', this.inputChanged);
+      this.el!.addEventListener(this.valueChangeEvent, this.notifyVaadinComponentOfChange);
+    } else {
+      this.el!.addEventListener('change', this.inputValueChanged);
     }
   }
 
-  creditTypeChanged() {
-    const _this = this;
+  removeListeners() {
+    console.log(this.logPrefix + "Removing previous change listeners");
 
-    return (e: Event) => {
-      const value = (e.target as HTMLInputElement).value;
-      const creditCardType = getCreditCardType(value);
-      (_this as any).$server.onCreditCardChanged(creditCardType);
-    };
+    if (!this.el) {
+      console.error(this.logPrefix + "Cannot remove listeners because this.el is null");
+      return;
+    }
+
+    this.el!.removeEventListener('input', this.creditTypeChanged);
+    this.el!.removeEventListener('input', this.inputChanged);
+    this.el!.removeEventListener(this.valueChangeEvent, this.notifyVaadinComponentOfChange);
+    this.el!.removeEventListener('change', this.inputValueChanged);
   }
 
-  inputChanged() {
-    const _this = this;
+  private creditTypeChanged = (e: Event) => {
+    // ignore event if format type is not credit card
+    // if (this.formatType != 'creditCard') {
+    //   return;
+    // }
 
-    return (e: Event) => {
-      const value = (e.target as HTMLInputElement).value;
-      const formattedValue = this.formatValue(value);
+    console.log(this.logPrefix + 'creditTypeChanged'); console.log(e);
+    const value = (e.target as HTMLInputElement).value;
+    const creditCardType = getCreditCardType(value);
+    (this as any).$server.onCreditCardChanged(creditCardType);
+  }
 
-      (_this.parentElement as any).value = formattedValue;
-      if (formattedValue) {
-        this.el!.value = formattedValue;
-      }
-      (_this.parentElement as any)._onChange(e);
+  private inputChanged = (e: Event) => {
+    console.log(this.logPrefix + 'inputChanged'); console.log(e);
+    const value = (e.target as HTMLInputElement).value;
+    const formattedValue = this.formatValue(value);
 
+    console.log(this.logPrefix + "InputValue[" + value + "] FormattedValue[" + formattedValue + "]");
+
+    (this.parentElement as any).value = formattedValue;
+    if (formattedValue) {
+      this.el!.value = formattedValue;
     }
+  }
+
+  notifyVaadinComponentOfChange = (e: Event) => {
+    console.log(this.logPrefix + "Notifying vaadin component of change");
+    (this.parentElement as any)._onChange(e);
   }
 
   formatValue(value: string) {
@@ -100,35 +135,77 @@ class TextfieldFormatter extends HTMLElement {
     return formattedValue;
   }
 
-  inputValueChanged() {
-    const _this = this;
-
-    return (e: Event) => {
-      const value = (e.target as HTMLInputElement).value;
-      const formattedValue = this.formatValue(value);
-      (_this.parentElement as any).value = formattedValue;
-      // should work specifically for the credit card
-      if (stripDelimiters(value, DefaultCreditCardDelimiter) !== unformatCreditCard(formattedValue)) {
-        console.error('Value has been striped');
-      }
-    };
-  }
+  private inputValueChanged = (e: Event) => {
+    console.log(this.logPrefix + 'inputValueChanged'); console.log(e);
+    const value = (e.target as HTMLInputElement).value;
+    const formattedValue = this.formatValue(value);
+    (this.parentElement as any).value = formattedValue;
+    // should work specifically for the credit card
+    if (stripDelimiters(value, DefaultCreditCardDelimiter) !== unformatCreditCard(formattedValue)) {
+      console.error('Value has been striped');
+    }
+  };
 
   updateConf(configuration: any, formatType: 'creditCard' | 'general' | 'numeral' | 'date'  | 'time') {
+    console.log(this.logPrefix + 'updateConf'); console.log(formatType); console.log(configuration);
     this.formatType = formatType;
     this.configuration = configuration;
-    console.log('CleaveZenFormatter: updateConf'); console.log(this.configuration);
     if (this.cursorTracker) {
       this.cursorTracker()
     }
     this.cursorTracker = registerCursorTracker({ input: this.el!, delimiter: this.configuration.delimiter });
+
+    // if we're already initialized, fire change event to force immediate update with new cleave config
+    if (this.el) {
+      this.fireInputEvent(true);
+    }
   }
+
   updateValueChangeEvent(valueChangeEvent: string) {
-    this.valueChangeEvent = valueChangeEvent;
-    console.log('CleaveZenFormatter: updateValueChangeEvent'); console.log(this.valueChangeEvent);
+    console.log(this.logPrefix + 'updateValueChangeEvent'); console.log(valueChangeEvent);
+
+    // redo listeners with new event type
+    if (this.el) {
+      this.removeListeners();
+      this.valueChangeEvent = valueChangeEvent;
+      this.addListeners();
+    }
+  }
+
+  fireInputEvent(notifyVaadinComponent: boolean) {
+    console.log(this.logPrefix + 'Manually firing an input event');
+
+    // todo do we need to differ the event type based on whether its a vaadin text component?
+    // let eventType = '';
+    // if (['VAADIN-TEXT-FIELD', 'VAADIN-TEXT-AREA'].includes(this.parentElement!.tagName.toUpperCase())) {
+    //   eventType = 'input';
+    // } else {
+    //   eventType = 'change';
+    // }
+
+    const event = new Event('input', {
+      bubbles: true,
+      cancelable: false,
+      composed: true
+    });
+
+    console.log(this.logPrefix + "New Event:");
+    console.log(event);
+
+    const isEventDispatched = this.el!.dispatchEvent(event);
+    console.log(this.logPrefix + 'isEventDispatched = ' + isEventDispatched);
+
+    if (notifyVaadinComponent) {
+      this.notifyVaadinComponentOfChange(event);
+    }
   }
 
   disconnectedCallback() {
+    console.log(this.logPrefix + "disconnectedCallback");
+
+    // remove event listeners
+    this.removeListeners();
+
     if (this.cursorTracker) {
       this.cursorTracker()
     }
