d94a822f5db197d5c942d31bf4e0e48cd9a7ef9f
Drew Harvey <drew@vaadin.com>
2024-09-27 14:49:43 -0700

    - updating to multi-module project with separate addon module and demo module (ITs are in demo app) - adding dockerfile

---
+++ cleave-zen-formatter-demo/src/main/java/org/vaadin/cleavezenformatter/Configurator.java
@@ -0,0 +1,8 @@
+//package cleavezenformatter;
+//
+//import com.vaadin.flow.component.page.AppShellConfigurator;
+//import com.vaadin.flow.theme.Theme;
+//
+//@Theme
+//public class Configurator implements AppShellConfigurator {
+//}
